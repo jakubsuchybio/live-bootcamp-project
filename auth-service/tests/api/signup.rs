50fa9ff2c753dfa714b4907b4b1721eeee314534
Kuba <6567287+jakubsuchybio@users.noreply.github.com>
2025-04-18 18:14:05 +0200

    add workspace

--- auth-service/tests/api/signup.rs
+++ auth-service/tests/api/signup.rs
@@ -23,17 +23,31 @@ async fn should_return_201_if_valid_input() {
 async fn should_return_400_if_invalid_input() {
     // Arrange
     let app = TestApp::new().await;
+    let invalid_emails = [
+        "",                    // empty
+        "a@a.cz",              // less than 8 characters
+        "ahoj",                // not containing @
+        "ahoj_ahoj_ahoj_ahoj", // not containing @
+    ];
 
-    // Act
-    let response = app
-        .post_signup(&serde_json::json!({
-            "password": "password123",
-            "requires2FA": true
-        }))
-        .await;
+    for email in invalid_emails.iter() {
+        // Act
+        let response = app
+            .post_signup(&serde_json::json!({
+                "email": email,
+                "password": "password",
+                "requires2FA": true
+            }))
+            .await;
 
-    // Assert
-    assert_eq!(response.status().as_u16(), 400);
+        // Assert
+        assert_eq!(
+            response.status().as_u16(),
+            400,
+            "Failed for password: {}",
+            email
+        );
+    }
 }
 
 #[tokio::test]
@@ -42,8 +56,9 @@ async fn should_return_409_if_email_already_exists() {
     let app = TestApp::new().await;
     let random_email = get_random_email();
 
-    // Act - First signup
-    let response = app
+    // Act
+    // First signup
+    let first_response = app
         .post_signup(&serde_json::json!({
             "email": random_email,
             "password": "password123",
@@ -51,11 +66,8 @@ async fn should_return_409_if_email_already_exists() {
         }))
         .await;
 
-    // Assert - First signup successful
-    assert_eq!(response.status().as_u16(), 201);
-
-    // Act - Second signup with same email
-    let response = app
+    // Second signup with same email
+    let second_response = app
         .post_signup(&serde_json::json!({
             "email": random_email,
             "password": "password123",
@@ -63,8 +75,11 @@ async fn should_return_409_if_email_already_exists() {
         }))
         .await;
 
-    // Assert - Second signup fails with conflict
-    assert_eq!(response.status().as_u16(), 409);
+    // Assert
+    // First signup successful
+    assert_eq!(first_response.status().as_u16(), 201);
+    // Second signup fails with conflict
+    assert_eq!(second_response.status().as_u16(), 409);
 }
 
 #[tokio::test]
@@ -93,7 +108,7 @@ async fn should_return_422_if_malformed_input() {
     for test_case in test_cases.iter() {
         // Act
         let response = app.post_signup(test_case).await;
-        
+
         // Assert
         assert_eq!(
             response.status().as_u16(),
