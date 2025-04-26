98a281618676515eac3bf13b4f558b5b133fcfcd
Kuba <6567287+jakubsuchybio@users.noreply.github.com>
2025-04-27 00:19:19 +0200

    fix https removal of the cookie? 2nd

--- auth-service/src/routes/logout.rs
+++ auth-service/src/routes/logout.rs
@@ -1,5 +1,6 @@
-use axum::{extract::State, http::StatusCode, response::IntoResponse};
-use axum_extra::extract::{cookie::Cookie, CookieJar};
+use axum::{extract::State, http::StatusCode, response::IntoResponse, Extension};
+use axum_extra::extract::{cookie, cookie::Cookie, CookieJar};
+use std::time::Duration;
 
 use crate::{
     domain::AuthAPIError,
@@ -8,16 +9,17 @@ use crate::{
 };
 
 pub async fn logout(
+    Extension(prefix): Extension<String>,
     State(state): State<AppState>,
     jar: CookieJar,
 ) -> (CookieJar, Result<impl IntoResponse, AuthAPIError>) {
     // Retrieve JWT cookie from the `CookieJar`
-    let Some(cookie) = jar.get(JWT_COOKIE_NAME) else {
+    let Some(cookie2) = jar.get(JWT_COOKIE_NAME) else {
         return (jar, Err(AuthAPIError::MissingToken));
     };
 
     // Validate JWT
-    let token = cookie.value().to_owned();
+    let token = cookie2.value().to_owned();
     let Ok(_claims) = validate_token(&token, state.banned_token_store.clone()).await else {
         return (jar, Err(AuthAPIError::InvalidToken));
     };
@@ -27,7 +29,10 @@ pub async fn logout(
 
     println!("Before removal: {:?}", jar);
     // Delete JWT cookie from the `CookieJar`
-    let cloned_cookie = cookie.clone();
+    let mut cloned_cookie = cookie2.clone();
+    cloned_cookie.set_path("/");
+    cloned_cookie.set_max_age(time::Duration::new(0, 0));
+
     let jar = jar.remove(cloned_cookie);
     println!("After removal: {:?}", jar);
 
